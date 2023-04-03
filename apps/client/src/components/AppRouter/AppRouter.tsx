83f146ca4d05f257da9caf2a13c496c2d7f1e20b
bublinec <jurajbublinec@gmail.com>
2023-04-03 18:24:32 +0200

    chore(client): enable index redirect

--- apps/client/src/components/AppRouter/AppRouter.tsx
+++ apps/client/src/components/AppRouter/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Cart } from '../../pages/Cart';
 import { Order } from '../../pages/Order';
 import { Products } from '../../pages/Products';
@@ -6,7 +6,7 @@ import { Products } from '../../pages/Products';
 export function AppRouter() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Navigate to="/products" />} /> */}
+      <Route path="/" element={<Navigate to="/products" />} />
       <Route path="/products" element={<Products />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/order" element={<Order />} />
